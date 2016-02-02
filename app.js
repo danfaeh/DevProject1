7b14278f7d2cc3860baddafe145b6afd80a2470a
nscalf561 <scalf.nicholas@gmail.com>
2016-02-02 15:39:19 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+$(function() {
+
+});
+
+function makeField () {
+
+}
+
+function addOnClicks () {
+
+}
+
+function initValues () {
+
+}
+
+function enemyMovement () {
+
+}
+
+function towerAction () {
+
+}
+
+function gameFlow () {
+
+}
+
+/* create the enemy prototype and the tower prototype */
