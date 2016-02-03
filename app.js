ff312d48037d4a435b92de23fc8c2655ba2cff2b
nscalf561 <scalf.nicholas@gmail.com>
2016-02-02 17:25:26 -0800

    finds out what doesn't work

--- app.js
+++ app.js
@@ -16,14 +16,45 @@ function addOnClicks () {
 	//we're looping through the nodelist and attributing an onclick event to all of them
 	for (var i = 0; i < boxNodeList.length; i++) {
 		
-		//NOTE TO SELF: this is properly attributing an onclick event to every box
+		//initializes all boxes with fillFlag, which will be used to see if its available to
+		//place a tower on.  True == available, false == not available.  The track will be filled
+		//in onBoxClick array
+//		boxNodeList[i].fillFlag = true;
+
 		//upon clicking any box in the grid, the "onBoxClick" event is called
-		$(boxNodeList[i]).click(onBoxClick);
+		$(boxNodeList[i]).click(onBoxClick(e));
 
 	}
 }
 
-function onBoxClick () {
+
+//THIS IS NOT WORKING, YOU NEED TO MAKE AN OBJECT AND LINK THAT OBJECT TO EACH
+//ONE OF THE DIVS 
+
+function onBoxClick (e) {
+	//this will give us a nodelist of the 
+	var trackNL = document.querySelectorAll(".track");
+	var boxNodeList = document.querySelectorAll(".box");
+
+	//we're looping through the nodelist and attributing an onclick event to all of them
+	for (var i = 0; i < boxNodeList.length; i++) {
+		
+		//initializes all boxes with fillFlag, which will be used to see if its available to
+		//place a tower on.  True == available, false == not available.  The track will be filled
+		//in onBoxClick array
+		boxNodeList[i].fillFlag = true;
+	}
+		console.log(boxNodeList);
+
+
+	//loops through the boxes that will be the track and marks that they are occupied so no
+	//towers can build on there
+	for (var x = 0; x < trackNL.length; x++) {
+
+		trackNL[x].fillFlag = false;
+
+	}
+	console.log(trackNL);
 
 }
 
@@ -48,6 +79,7 @@ function buildChoice () {
 
 }
 
+//use this to check if something can be built here ---> if buildable return true
 function fillCheck () {
 
 }
